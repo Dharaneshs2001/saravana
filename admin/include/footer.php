02435667f990abe2a331396082ffd4335602e666
dharanesh <dharaneshs2001@gmail.com>
2022-08-11 18:46:40 +0530

    commit

---
+++ admin/include/footer.php
@@ -0,0 +1,7 @@
+	<div class="footer"  style="background-color: lightgreen;">
+		<div class="container">
+			 
+
+			<b style="color:black;" class="copyright">&copy; 2022 Saravana Tiles. </b> All rights reserved.
+		</div>
+	</div>
---
+++ includes/footer.php
@@ -0,0 +1,88 @@
+<footer id="footer" class="footer" style="background-color: green ">
+	  <div class="links-social inner-top-sm">
+        <div class="container">
+            <div class="row">
+            	<div class="col-xs-12 col-sm-6 col-md-4">
+            		 <!-- ============================================================= CONTACT INFO ============================================================= -->
+                    <div class="contact-info">
+                        <div class="footer-logo">
+                            <div class="logo">
+                                <a href="index.php">
+                                    
+                    
+                                </a>
+                            </div><!-- /.logo -->
+                        
+                        </div><!-- /.footer-logo -->
+
+                         <div class="module-body m-t-20">
+                            <p class="about-us"style=" color:white"> Saravana Tiles is the one of the leading tiles show room in Salem .</p>
+                        </div><!-- /.module-body -->
+
+                    </div><!-- /.contact-info -->
+                </div><!-- /.col -->
+
+            	<div class="col-xs-12 col-sm-6 col-md-4">
+            		 <!-- ============================================================= CONTACT TIMING============================================================= -->
+                <div class="contact-timing">
+                	<div class="module-heading">
+                		<h4 class="module-title" style="color: white;">Contact</h4>
+                	</div><!-- /.module-heading -->
+
+                	<div class="module-body outer-top-xs">
+                		<div class="table-responsive">
+                            
+                                        <h3 style="color:white;">0427 4040427</h3>
+                           
+                		</div><!-- /.table-responsive -->
+                	</div><!-- /.module-body -->
+                </div><!-- /.contact-timing -->
+                <!-- ============================================================= CONTACT TIMING : END ============================================================= -->            	</div><!-- /.col -->
+
+            	<div class="col-xs-12 col-sm-6 col-md-4">
+            		 <!-- ============================================================= INFORMATION============================================================= -->
+                    <div class="contact-information">
+                    	<div class="module-heading">
+                    		<h4 class="module-title" style="color:white;">Address</h4>
+                    	</div><!-- /.module-heading -->
+
+                    	<div class="module-body outer-top-xs">
+                            <ul class="toggle-footer" style="">
+                                <li class="media">
+                                    <div class="pull-left">
+                                         <span class="icon fa-stack fa-lg">
+                                          <i class="fa fa-circle fa-stack-2x"></i>
+                                          <i class="fa fa-map-marker fa-stack-1x fa-inverse"></i>
+                                        </span>
+                                    </div>
+                                    <div class="media-body">
+                                        <p style="color:white">ARRS 5 theatre road, new bus stand, Salem, 636 004, Tamilnadu</p>
+                                        <a href="https://goo.gl/maps/d7hoxy9x4LQKSHWC9" target="_blank">
+    <button style="color:black;background-color: white;">View Location</button>
+  </a> 
+                                    </div>
+                                </li>
+
+                                  
+
+                                  <li class="media">
+                                    <div class="pull-left">
+                                         <span class="icon fa-stack fa-lg">
+                                          <i class="fa fa-circle fa-stack-2x"></i>
+                                          <i class="fa fa-envelope fa-stack-1x fa-inverse"></i>
+                                        </span>
+                                    </div>
+                                    <div class="media-body">
+                                        <span><a href="#" style="color:white">saravanatiles22@gmail.com</a></span>
+                                    </div>
+                                </li>
+                                  
+                                </ul>
+                        </div><!-- /.module-body -->
+                    </div><!-- /.contact-timing -->
+<!-- ============================================================= INFORMATION : END ============================================================= -->            	</div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container -->
+    </div><!-- /.links-social -->
+
+    
